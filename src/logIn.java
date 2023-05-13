6326f2828de12efafd39db9b58b24362ac846508
Sreejesh08 <118043443+Sreejesh08@users.noreply.github.com>
2023-05-13 12:49:06 +0530

    update

--- src/logIn.java
+++ src/logIn.java
@@ -5,38 +5,46 @@ import java.util.Scanner;
 public class logIn {
 
     static Scanner scan = new Scanner(System.in);
+    static final String ANSI_RESET = "\u001B[0m";
 
     // public static void main(String[] args) throws SQLException {
-    public void takeInput() throws SQLException { 
+    public void takeInput(String type) throws SQLException {
         boolean loggedin = false;
-        while (loggedin == false) {
+        System.out.println("\u001B[35m");
         System.out.println("Welcome");
-        System.out.print("Enter your email: ");
-        String email = scan.nextLine();
-        if (credCheck("email", email)) {
-            System.out.println("Enter your Password");
-            String password = scan.nextLine();
-            if (credCheck("password", password)) {
-                loggedin = true;
-                System.out.println("You are Logged in");
+        while (loggedin == false) {
+            System.out.print("Enter your email: ");
+            String email = scan.nextLine();
+            if (credCheck("email", email, type)) {
+                System.out.println("Enter your Password");
+                String password = scan.nextLine();
+                if (credCheck("password", password, type)) {
+                    loggedin = true;
+                    System.out.println();
+                    System.out.println("You are Logged in");
+                } else {
+                    System.out.println();
+                    System.out.println("Wrong Password");
+                    System.out.println();
+                    continue;
+                }
+            } else {
+                System.out.println();
+                System.out.println("Wrong Username");
+                System.out.println();
+                continue;
             }
-            else {
-                System.out.println("Wrong Password");
-                continue;}
-        }
-        else {
-            System.out.println("Wrong Username");
-            continue;}
 
         }
-        
+        System.out.print(ANSI_RESET);
 
     }
 
-    protected boolean credCheck(String col, String call) throws SQLException {
+    protected boolean credCheck(String col, String call, String type) throws SQLException {
         boolean found = false;
         dbConnection dbcon = new dbConnection();
-        String query = "select " + col + " from login";
+        String query = "select " + col + " from login where emp_id in (select emp_id from employees " +
+                "where emp_type = '" + type + "');";
         ResultSet res = dbcon.select(query);
         while (res.next()) {
             if (res.getString(1).equals(call)) {
@@ -48,5 +56,5 @@ public class logIn {
         dbcon.disconnectFromDatabase();
         return found;
     }
-    
+
 }
